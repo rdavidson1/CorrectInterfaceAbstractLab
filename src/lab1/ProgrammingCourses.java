fceba5c42c4ca0af9b0c33d0458ea2005455f7f7
Ryan <Ryan@192.168.0.2>
2014-09-04 11:08:41 -0500

    Getting Started Commit 2

--- src/lab1/ProgrammingCourses.java
+++ src/lab1/ProgrammingCourses.java
@@ -14,4 +14,34 @@ public abstract class ProgrammingCourses {
     private String courseName;
     private String courseNumber;
     private double credits;
+
+    public ProgrammingCourses(String courseName, String courseNumber, double credits) {
+        this.courseName = courseName;
+        this.courseNumber = courseNumber;
+        this.credits = credits;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    public double getCredits() {
+        return credits;
+    }
+
+    public void setCredits(double credits) {
+        this.credits = credits;
+    }
 }
